4ca63c62470daa48c22fdecfb622aa1419aaa2aa
Vlad Patryshev <vpatryshev@gmail.com>
2022-01-19 21:28:27 -0500

    Works ok; style good. Next:  - have some docs  - produce some examples  - iterate

--- Goodstein/src/main/scala/scalakittens/Goodstein.scala
+++ Goodstein/src/main/scala/scalakittens/Goodstein.scala
@@ -4,98 +4,50 @@ import Numbers._
 import scala.util.parsing.combinator._
 
 object Goodstein {
-  def const(n: BigInt) = Towers(n)
-  val Const1 = const(Big1)
-  val ConstK = Towers(Big0, Const1 -> Big1)
+  
+  class Parser extends JavaTokenParsers {
+    def digits: Parser[BigInt] = """\d+""".r ^^ { n => BigInt(n) }
 
-  def single(expr: Towers) = Towers(Big0, expr -> Big1)
-  
-  def mult(q: BigInt, single: Towers) = Towers(Big0, single.contents.head._1 -> q)
-  
-  class T extends JavaTokenParsers {
-    def tap[X](what: String, x: X) = {
-//      println(s"got $what (${x.getClass}) $x")
-      x
+    /**
+     * A single digit can denote a degree; it's not wrapped in curlies, e.g. k^7
+     * @return a regex parser
+     */
+    def smallDegree: Parser[Int] = """\d""".r ^^ {(n: String) => n.toInt }
+
+    /**
+     * A BigInt number that serves as a constant term
+     * @return
+     */
+    def constantTerm: Parser[GT] = digits ^^ { Const }
+
+    def expr: Parser[GT] = repsep(term, "+") ^^ {
+      terms => 
+        val const = terms.map(_.constantTerm).fold(Big0)(_+_)
+        GT(const, terms flatMap (_.terms))
+    }
+
+    def coefficient: Parser[BigInt] = (digits ~ "×") ^^ { 
+      case n ~ _ => n
+    }
+
+    def power: Parser[GT] = ("k^{"~expr~"}" | "k^k" | ("k^"~smallDegree) | "k") ^^ {
+      case "k^k" => Power(GT.K)
+      case "k^{"~(expr:GT)~"}" => Power(expr)
+      case "k^" ~ (digit: Int) => Power(Const(digit))
+      case "k" => GT.K
     }
     
-    def digit: Parser[String] = """\d""".r
-
-    def digits: Parser[String] = """\d+""".r
-    
-    def smallNumber: Parser[Int] = digit ^^ {(n: String) => tap("smallint", n.toInt)}
-
-    def number: Parser[Towers] = digits ^^ { n => tap("bigint", Towers(BigInt(n))) }
-
-    def expr: Parser[Towers] = repsep(term, "+") ^^ {
-      case list => tap("for expr:", list);
-        val const = list.map(_.tail).fold(Big0)(_+_)
-        val grouped = list.flatMap(_.contents).groupBy(_._1)
-        val withCounts = grouped.map {
-          case (t, group) => t -> group.map(_._2).fold(Big0)(_+_)
-        }.toList
-        val sorted = withCounts.sortBy(_._1).reverse
-//        println(s"Before and after sorting:\n$withCounts\n$sorted")
-        val result = Towers(const, sorted :_*)
-        tap("result: ", result)
-    }
-
-    def q: Parser[BigInt] = (digits ~ "×") ^^ { 
-      case n ~ _ => tap("q", BigInt(n))
-    }
-
-    def power: Parser[Towers] = ("k^{"~expr~"}" | "k^k" | ("k^"~smallNumber) | "k") ^^ {
-      case "k^k" => tap("pure", "k^k")
-        Towers(0, ConstK -> Big1)
-      case "k^{"~(expr:Towers)~"}" => tap("something", expr); tap("resulting: ", single(expr))
-//      case "k^9" => tap("k9", single(Towers(BigInt(9))))
-      case "k^" ~ (digit: Int) => tap("kdigit", single(Towers(BigInt(digit))))
-      case "k" => tap("singleton", "k"); ConstK
-    }
-    def term: Parser[Towers] = (power | (q ~ power) | number) ^^ {
-//      case number => tap("plain number", _)
-      case (q: BigInt) ~ (power:Towers) =>
-        tap("quotient", q); tap("power", power); tap("qx?", mult(q, power))
-      case p: Towers => tap("a term", p)
-    }
-
-    def asTerm(ex: String): Towers =  {
-      println(s"Trying <term> $ex")
-      val parsed = parser.parseAll(parser.term, ex)
-      println(parsed)
-      println("-----------------------")
-      parsed.get
+    def term: Parser[GT] = (power | (coefficient ~ power) | constantTerm) ^^ {
+      case (q: BigInt) ~ (t:Term) => t * q
+      case c: Const => c
+      case t: Term => t
     }
     
-    def apply(ex: String): Towers = {
-//      println(s"Running $ex")
-      val parsed = parser.parseAll(parser.expr, ex)
-//      println(parsed)
-//      println("-----------------------")
-      parsed.get
-    }
-
+    def apply(ex: String): GT = parseAll(expr, ex).get
   }
-
-
-  val parser = new T
-  
-  def run(ex: String): Unit = {
-    parser.apply(ex)
-  }
-  
-  def main(args: Array[String]): Unit =
-  {
-    run("256")
-    run("k^k")
-    run("k^{k}")
-    run("22 + k")
-    run("123 + 321 + k + k + k^{k} + k^k + 5×k")
-    run("10×k^{1+2×k^{13×k}} + k^{k}")
-  }
-
 
   // e.g. 3\(\times\)(132^{132}) + 3\(\times\)(132^3) + 2\(\times\)(132^2) + 3\(\times\)132 + 131
-  def latex2code(latex: String): Towers = {
-    Towers(latex.replace("""\(\times\)""", "×"))
+  def latex2code(latex: String): GT = {
+    GT(latex.replace("""\(\times\)""", "×"))
   }
 }
--- Goodstein/src/test/scala/scalakittens/GoodsteinTest.scala
+++ Goodstein/src/test/scala/scalakittens/GoodsteinTest.scala
@@ -2,40 +2,50 @@ package scalakittens
 import org.scalatest.flatspec.AnyFlatSpec
 import Numbers._
 import org.scalatest.matchers.should.Matchers
-import scalakittens.Goodstein._
 
 class GoodsteinTest extends AnyFlatSpec with Matchers {
-  val _5: Towers = Towers(5)
-  val _8: Towers = Towers(8)
-  val twoByPowerOfThreeByTen: Towers = Towers(2, _8 -> BigInt(3))
-  val sample: Towers = Towers(1, _5 -> 3, twoByPowerOfThreeByTen -> 5)
+  lazy val _5: GT = Const(5)
+  lazy val _8: GT = Const(8)
+  lazy val twoByPowerOfThreeByTen: GT = GT(2, _8 -> BigInt(3) :: Nil)
+  lazy val sample: GT = GT(1, List(_5 -> 3, twoByPowerOfThreeByTen -> 5))
 
   "tower" should "eval" in {
       sample.eval(1) shouldEqual 9
       twoByPowerOfThreeByTen.eval(2) shouldEqual 770
-      Towers(3, _5->2, _8->6).eval(2) shouldEqual 1603
+      GT(3, List(_5->2, _8->6)).eval(2) shouldEqual 1603
       val bigOne = sample.eval(3)
       bigOne.bitLength shouldEqual 31203
       toDoubleString(bigOne, 5) shouldEqual """6.77437×10^{9392}"""
     }
   "tower" should "toString" in {
-    Towers(0, ConstK -> 1).toString shouldEqual "k^k"
+    GT(0, List(GT.K -> 1)).toString shouldEqual "k^k"
     twoByPowerOfThreeByTen.toString shouldEqual "3×k^8 + 2"
-    sample.toString shouldEqual "3×k^5 + 5×k^{3×k^8 + 2} + 1"
-    Const1.toString shouldEqual "1"
-    ConstK.toString shouldEqual "k"
+    sample.toString shouldEqual "5×k^{3×k^8 + 2} + 3×k^5 + 1"
+    GT.Const_1.toString shouldEqual "1"
+    GT.K.toString shouldEqual "k"
   }
   
   "tower" should "parse" in {
     def check(s: String, expected: String = null): Unit = {
-      Towers(s).toString shouldEqual Option(expected).getOrElse(s)
+      GT(s).toString shouldEqual Option(expected).getOrElse(s)
     }
 
+    check("256")
+    check("k^k")
+    check("k^{k}", "k^k")
+    check("22+k", "k + 22")
+    check("123 + 321 + k + k + k^{k} + k^k + 5×k", "2×k^k + 7×k + 444")
+    check("10×k^{1+2×k^{13×k}} + k^{k}", "10×k^{2×k^{13×k} + 1} + k^k")
+
+    check("3×k^3 + 3×k^2 + 3×k + 3")
+    check("k^{k} + k^k", "2×k^k")
     check("k")
     check("5×k^9")
     check("5×k^k")
     check("5×k")
     check("k^k")
+    check("k^k + k^k", "2×k^k")
+    check("k^{0 + k} + k^{k + 0}", "2×k^k")
     check("k^9")
     check("256")
     check("k^{k}", "k^k")
@@ -46,13 +56,16 @@ class GoodsteinTest extends AnyFlatSpec with Matchers {
 //
   //  val n263 = 3 * pow(263, 263) + 3 * pow(263, 3) + 2 * pow(263, 2) + 30 * 263  
   "tower" should "dec" in {
-    Towers("k^k").dec(4) shouldEqual Towers("3×k^3 + 3×k^2 + 3×k + 3")
-    Towers("k").dec(7) shouldEqual Towers("6")
-    Towers("10×k").dec(10) shouldEqual(Towers("9×k + 9"))
-    Towers("17").dec(10) shouldEqual(Towers("16"))
-    Towers("5×k").dec(8) shouldEqual Towers("4×k + 7")
-    Towers("3×k^2").dec(10) shouldEqual Towers("2×k^2 + 9×k + 9")
-    Towers("3×k^2 + 8").dec(10) shouldEqual Towers("3×k^2 + 7")
-    Towers("2×k^k").dec(3) shouldEqual Towers("k^k + 2×k^2 + 2×k + 2")
+    val source1 = GT("k^k")
+    val expected1 = GT("3×k^3 + 3×k^2 + 3×k + 3")
+    val sample1 = source1.dec(4)
+    sample1 shouldEqual expected1
+    GT("k").dec(7) shouldEqual GT("6")
+    GT("10×k").dec(10) shouldEqual GT("9×k + 9")
+    GT("17").dec(10) shouldEqual GT("16")
+    GT("5×k").dec(8) shouldEqual GT("4×k + 7")
+    GT("3×k^2").dec(10) shouldEqual GT("2×k^2 + 9×k + 9")
+    GT("3×k^2 + 8").dec(10) shouldEqual GT("3×k^2 + 7")
+    GT("2×k^k").dec(3) shouldEqual GT("k^k + 2×k^2 + 2×k + 2")
   }
 }
